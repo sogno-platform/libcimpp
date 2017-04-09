57dd88f936d1b97689c5d66f37af401c07bfc6dd
Maximilian Kissgen <maximilian.kissgen@rwth-aachen.de>
2017-04-09 15:18:27 +0200

    Implemented check for the ContentHandler to parse the given rdfID into an IdentifiedObject's K mRID

--- src/CIMContentHandler.cpp
+++ src/CIMContentHandler.cpp
@@ -98,6 +98,12 @@ void CIMContentHandler::startElement(const std::string &namespaceURI, const std:
 		else // object does not exist -> create object
 		{
 			BaseClass* BaseClass_ptr = CIMFactory::CreateNew(qName);
+
+			//Check if created Object is IdentifiedObject and place rdf_id into mRID
+			if(IEC61970::Base::Core::IdentifiedObject* idOb = dynamic_cast<IEC61970::Base::Core::IdentifiedObject*>(BaseClass_ptr))
+			{
+				(*idOb).mRID = rdf_id;
+			}
 			RDFMap->emplace(rdf_id, BaseClass_ptr);
 			objectStack.push(BaseClass_ptr);
 			Objects->push_back(BaseClass_ptr);
