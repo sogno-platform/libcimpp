6bf615e62da85825b175c7812add91fb0ee3630a
Philipp Dreßen <philipp@drehsen.net>
2017-11-17 16:11:03 +0100

    Switched to docker:git image for mirroring

--- ci/mirrorToGithub.sh
+++ ci/mirrorToGithub.sh
@@ -1,10 +1,9 @@
 # install ssh-key
 mkdir -p $HOME/.ssh
-chmod +rw $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa.pub
+chmod -R +rw $HOME/.ssh
 echo "$DEPLOY_KEY" > $HOME/.ssh/id_rsa
-chmod 400 $HOME/.ssh/id_rsa
 echo "$DEPLOY_KEY_PUB" > $HOME/.ssh/id_rsa.pub
-chmod 400 $HOME/.ssh/id_rsa.pub
+chmod -R 400 $HOME/.ssh
 
 # add  server host_key
 echo "$GITLAB_HOSTKEY" >> $HOME/.ssh/known_hosts
@@ -12,6 +11,7 @@ echo "$GITHUB_HOSTKEY" >> $HOME/.ssh/known_hosts
 
 # mirror repository from gitlab to github
 git clone --mirror git@git.rwth-aachen.de:acs/core/cim/cimpp/libcimpp.git
+
 cd libcimpp.git
 git remote add github git@github.com:RWTH-ACS/libcimpp.git
 git push --mirror github
