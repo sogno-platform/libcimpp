c2698b37da5fdbab82d68d97d975b6b3c233149c
Steffen Vogel <post@steffenvogel.de>
2018-02-26 09:53:00 +0100

    move static aliases from CSV files to hard coded header files

---
+++ src/AliasesAssignment.hpp
@@ -0,0 +1,2 @@
+dynamic_switch.insert(std::make_pair("cim:Terminal.sequenceNumber", &assign_ACDCTerminal_sequenceNumber));
+dynamic_switch.insert(std::make_pair("cim:Terminal.connected", &assign_ACDCTerminal_connected));
