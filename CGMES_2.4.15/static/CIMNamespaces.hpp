9d4efc26adca795eea3380da023411746c3920da
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-04-23 15:10:07 +0200

    Execute make in cim-codebase-generator submodule

---
+++ CGMES_2.4.15/static/CIMNamespaces.hpp
@@ -0,0 +1,14 @@
+#ifndef CIM_NAMESPACE_HPP
+#define CIM_NAMESPACE_HPP
+
+#include "IdentifiedObject.hpp"
+#include "IEC61970/IEC61970CIMVersion.h"
+
+namespace IEC61970 {
+	using CGMES::IEC61970CIMVersion;
+	namespace Base::Core {
+		using CGMES::IdentifiedObject;
+	}
+}
+
+#endif
