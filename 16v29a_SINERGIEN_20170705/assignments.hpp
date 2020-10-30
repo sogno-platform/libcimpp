f0d6950caf2eb71fc64f9f959daefdcbaf01b420
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2020-10-30 20:32:14 +0100

    remove sinergien files

--- 16v29a_SINERGIEN_20170705/assignments.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ASSIGN_HPP
-#define ASSIGN_HPP
-
-#include <string>
-#include "BaseClass.h"
-
-bool assign(BaseClass* CIMObj, const std::string& CIMAttrName, const std::string& Value);
-
-#endif // ASSIGN_HPP
